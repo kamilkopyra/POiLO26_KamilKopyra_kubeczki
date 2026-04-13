f8e86cd80a226cb4bae8e176a9341496192408db
kamilkopyra <LabPOIO-04@vm-krim.local>
2026-04-13 16:04:12 +0200

    wykonanie zadania 1 - utworzenie okna aplikacji

---
+++ MainWin.cpp
@@ -0,0 +1,16 @@
+#include "MainWin.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	POiIOkubeczki::MainWin form;
+	Application::Run(% form);
+	return 0;
+
+}
+
---
+++ MainWin.h
@@ -0,0 +1,67 @@
+#pragma once
+
+namespace POiIOkubeczki {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o MainWin
+	/// </summary>
+	public ref class MainWin : public System::Windows::Forms::Form
+	{
+	public:
+		MainWin(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyœæ wszystkie u¿ywane zasoby.
+		/// </summary>
+		~MainWin()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Metoda wymagana do obs³ugi projektanta — nie nale¿y modyfikowaæ
+		/// jej zawartoœci w edytorze kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainWin::typeid));
+			this->SuspendLayout();
+			// 
+			// MainWin
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"MainWin";
+			this->Text = L"Program kubeczki";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
